4f4dbcfd2099b796a684644ca93bbc932f00642c
Stas <stanislavzebek@mail.ru>
2021-06-08 17:35:59 +0300

    Внесенеы небольшие изменения

--- src/App.js
+++ src/App.js
@@ -1,63 +1,71 @@
 import React, {useState} from 'react';
 
 import './App.css';
-import Search from "./components/Search/Search";
-import Content from "./components/Content/Content";
-import axios from 'axios'
-import Header from "./components/Header/Header";
+import Search from './components/Search/Search';
+import Content from './components/Content/Content';
+import axios from 'axios';
+import Header from './components/Header/Header'
 
-const myID='4319e25fb7c3d02491092a7b206ffc8c';
+const myID = '4319e25fb7c3d02491092a7b206ffc8c';
 
 function App() {
-    const [weatherNow,setWeatherNow] = useState({});
-    const [weatherLong,setWeatherLong] = useState('');
-    const [responseErr,setResponseErr]= useState('');
+    const [weatherNow, setWeatherNow] = useState({});
+    const [weatherLong, setWeatherLong] = useState('');
+    const [responseErr, setResponseErr] = useState('');
 
-    const getNowRequest = (data)=>{
+    const getNowRequest = (data) => {
         console.log(data);
-        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${data.city}&lang=ru&appid=${myID}&lang=ru`)
-            .then(res=> {
+        axios
+            .get(
+                `http://api.openweathermap.org/data/2.5/weather?q=${data.city}&lang=ru&appid=${myID}&lang=ru`
+            )
+            .then((res) => {
                 console.log(res);
                 setWeatherNow(res);
                 console.log(res.data);
                 setResponseErr('');
-                return res.data.name
+                return res.data.name;
             })
-            .then(town =>getFutureRequest(town))
-            .catch(error => {
+            .then((town) => getFutureRequest(town))
+            .catch((error) => {
                 if (error.response) {
                     console.log(error.response);
                     setResponseErr(error.response);
                 } else if (error.request) {
-                    console.log(error.request)
+                    console.log(error.request);
                 }
-            })
-    }
+            });
+    };
 
-    const getFutureRequest = (city)=>{
-        console.log(city)
-        axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ru&appid=${myID}&lang=ru`)
-            .then(res=> {
-                console.log('obj2',res.data);
-                setWeatherLong(res.data.list)
+    const getFutureRequest = (city) => {
+        console.log(city);
+        axios
+            .get(
+                `http://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ru&appid=${myID}&lang=ru`
+            )
+            .then((res) => {
+                console.log('obj2', res.data);
+                setWeatherLong(res.data.list);
             })
-            .catch(error => {
+            .catch((error) => {
                 if (error.response) {
-                    console.log(error.response)
+                    console.log(error.response);
                 } else if (error.request) {
-                    console.log(error.request)
+                    console.log(error.request);
                 }
-            })
-    }
+            });
+    };
 
     return (
         <div className="App">
-            <Header/>
-            <Search onFormSubmit={(city) => getNowRequest(city) } data = {responseErr.data}/>
-            {weatherLong && <Content {...weatherNow.data} list={weatherLong}/>}
+            <Header />
+            <Search
+                onFormSubmit={(city) => getNowRequest(city)}
+                data={responseErr.data}
+            />
+            {weatherLong && <Content {...weatherNow.data} list={weatherLong} />}
         </div>
-    )
+    );
 }
 
 export default App;
-
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,15 +1,13 @@
-import style from './Content.module.css'
-import React, {useState, useEffect} from 'react'
+import style from './Content.module.css';
+import React, {useState, useEffect} from 'react';
 
-import moment from 'moment';// подключаем библиотеку для удобной работы с датой
+import moment from 'moment'; // подключаем библиотеку для удобной работы с датой
 import 'moment/locale/ru';
-import {Map,YMaps} from "react-yandex-maps";
+import {Map, YMaps} from 'react-yandex-maps';
 // подключаем язык
 moment.locale('ru'); // выбираем нужный язык
 
-
-const countryObj = require('../../сountryBase/countryBase.json');//подключаем базу стран в JSON
-
+const countryObj = require('../../сountryBase/countryBase.json'); //подключаем базу стран в JSON
 
 let Context = ({main, weather, name, sys, wind, list, coord}) => {
     const finishToday = 8 - moment(list[0].dt * 1000).format('HH') / 3;
@@ -17,34 +15,42 @@ let Context = ({main, weather, name, sys, wind, list, coord}) => {
     const dayNow = new Date().getTime();
 
     const [start, setStart] = useState(0);
-    const [dayWeather, setDayWeather] = useState(moment().format('MMMM Do YYYY'));
+    const [dayWeather, setDayWeather] = useState(
+        moment().format('MMMM Do YYYY')
+    );
     const [finish, setFinish] = useState(finishToday);
     const [getTime, setGetTime] = useState(dayNow);
     // const [_coord, setCoord] = useState(coord);
-
 
     useEffect(() => {
         setFinish(finishToday);
         setStart(0);
         // console.log(moment(dayWeather).format('e'));
-    },[coord, finishToday]);
+    }, [coord, finishToday]);
 
     useEffect(() => {
         setDayWeather(moment(getTime).format('MMMM Do, YYYY'));
-    },[getTime]);
-
+    }, [getTime]);
 
     const day = () => {
-        if (moment(dayNow).format('D') === moment(list[start].dt * 1000).format('D')) return ('сегодня');
-        if (1 + +(moment(dayNow).format('D')) === +moment(list[start].dt * 1000).format('D')) return ('завтра');
-        return ' '
-    }
+        if (
+            moment(dayNow).format('D') ===
+            moment(list[start].dt * 1000).format('D')
+        )
+            return 'сегодня';
+        if (
+            1 + +moment(dayNow).format('D') ===
+            +moment(list[start].dt * 1000).format('D')
+        )
+            return 'завтра';
+        return ' ';
+    };
 
     const nextPrevWeather = (btn) => {
         switch (btn) {
             case 'prev':
                 setGetTime(getTime - 24 * 3600 * 1000);
-                if ((start - 8) < 0) {
+                if (start - 8 < 0) {
                     setStart(0);
                     setFinish(finishToday);
                 } else {
@@ -54,7 +60,7 @@ let Context = ({main, weather, name, sys, wind, list, coord}) => {
                 break;
             case 'next':
                 setGetTime(getTime + 24 * 3600 * 1000);
-                if ((finish + 8) < finishMax) {
+                if (finish + 8 < finishMax) {
                     setStart(finish);
                     setFinish(finish + 8);
                 } else {
@@ -63,106 +69,165 @@ let Context = ({main, weather, name, sys, wind, list, coord}) => {
                 }
                 break;
             default:
-                console.log( "Нет таких значений" );
+                console.log('Нет таких значений');
         }
-    }
+    };
     // console.log('start:', start)
     // console.log('finish:', finish)
     const countryName = () => {
-        console.log(countryObj.country[0] );
-        let objCont = countryObj.country.find(country => country.alpha2 === sys.country);
-        return (objCont) ? objCont.name : 'Неизвестно'
+        console.log(countryObj.country[0]);
+        let objCont = countryObj.country.find(
+            (country) => country.alpha2 === sys.country
+        );
+        return objCont ? objCont.name : 'Неизвестно';
     };
     // console.log(list);
-    const convertKtoC = T => {
+    const convertKtoC = (T) => {
         const temp = Math.ceil((T - 273.15) * 10) / 10;
-        return (temp < 0) ? <span>{Math.ceil((T - 273.15) * 10) / 10}&deg;</span> :
+        return temp < 0 ? (
+            <span>{Math.ceil((T - 273.15) * 10) / 10}&deg;</span>
+        ) : (
             <span>+{Math.ceil((T - 273.15) * 10) / 10}&deg;</span>
+        );
     };
     const directWind = (deg) => {
         if (deg < 22.5 || deg > 337.5) {
-            return 'С'
+            return 'С';
         } else if (deg > 22.5 && deg <= 67.5) {
-            return 'СВ'
+            return 'СВ';
         } else if (deg > 67.5 && deg <= 112.5) {
-            return 'В'
+            return 'В';
         } else if (deg > 112.5 && deg <= 157.5) {
-            return 'ЮВ'
+            return 'ЮВ';
         } else if (deg > 157.5 && deg <= 202.5) {
-            return 'Ю'
+            return 'Ю';
         } else if (deg > 202.5 && deg <= 247.5) {
-            return 'ЮЗ'
+            return 'ЮЗ';
         } else if (deg > 247.5 && deg <= 292.5) {
-            return 'З'
+            return 'З';
         } else if (deg > 292.5 && deg <= 337.5) {
-            return 'СЗ'
-        } else
-            return 'штиль'
-    }
+            return 'СЗ';
+        } else return 'штиль';
+    };
 
-    return <div className={style.contentContainer}>
-        <div className={style.mapContent}>
-            <YMaps >
-                <div className={style.map}>
-                    <Map className={style.map}  state ={{center: [coord.lat, coord.lon], zoom: 11}} defaultState={{center: [55.75, 37.57], zoom: 9}}>
-                    </Map>
-                </div>
-            </YMaps>
-        </div>
-
-        <div className={style.mainContent}>
-            <div className={style.text}> Страна: {countryName()} &emsp;  Город: {name} </div>
-            <div className={style.data}
-                 style={(+moment().format('e') === 5 || +moment().format('e') === 6) ? {color: 'red'} : {color: 'black'}}>{moment().format('MMMM Do YYYY, HH:mm')}</div>
-            <div className={style.today}>сейчас</div>
-            <div className={style.text}>Ветер: {wind.speed} м/с, {directWind(wind.deg)} </div>
-            <div className={style.text}>Давление: {Math.floor(main.pressure * 0.75)} мм рт. ст.</div>
-            <div className={style.text}>Влажность: {main.humidity} %</div>
-            <div className={style.text}>Температура:</div>
-            <div className={style.temperature}><span className={style.minTemp}>{convertKtoC(main.temp_min)}</span>
-                <span className={style.maxTemp}>{convertKtoC(main.temp_max)}</span></div>
-            <br/>
-            <div className={style.text}>Чувствуется: {convertKtoC(main.feels_like)} </div>
-        </div>
-
-        <div className={style.iconContent}>
-            <img src={`http://openweathermap.org/img/wn/${weather[0].icon}@4x.png`} className={style.iconImg} alt={'Нет картинки'}/>
-            <div className={style.textImg}>{weather[0].description}</div>
-        </div>
-
-        <div className={style.longWeatherContent}>
-            <div className={style.dayWeather}
-                 style={(+moment(list[start].dt * 1000).format('e') === 6 || (+moment(list[start].dt * 1000).format('e') === 0)) ? {color: 'red'} : {color: 'black'}}>{dayWeather}</div>
-            <div className={style.today}>
-                {day()}
-            </div>
-            <div className={style.longWeather}>
-                {(start !== 0) && <button onClick={() => nextPrevWeather('prev')}>&#10094;</button>}
-                {list.slice(start, finish).map((pieceTime, index) => {
-                    const match = moment(pieceTime.dt * 1000).format('ddd, D MMM');
-                    const time = moment(pieceTime.dt * 1000).format('HH:mm');
-                    return <div key={index} className={style.futureWeather}>
-                        <div>{match}</div>
-                        <div>{time}</div>
-                        <div>t: {convertKtoC(pieceTime.main.temp)} </div>
-                        <img src={`http://openweathermap.org/img/wn/${pieceTime.weather[0].icon}@2x.png`}
-                             className={style.miniIconImg} alt={pieceTime.weather[0].description}/>
+    return (
+        <div className={style.contentContainer}>
+            <div className={style.mapContent}>
+                <YMaps>
+                    <div className={style.map}>
+                        <Map
+                            className={style.map}
+                            state={{center: [coord.lat, coord.lon], zoom: 11}}
+                            defaultState={{center: [55.75, 37.57], zoom: 9}}
+                        />
                     </div>
-                })}
-                {(finish !== finishMax) && <button onClick={() => nextPrevWeather('next')}>&#10095;</button>}
+                </YMaps>
             </div>
 
+            <div className={style.mainContent}>
+                <div className={style.text}>
+                    {' '}
+                    Страна: {countryName()} &emsp; Город: {name}{' '}
+                </div>
+                <div
+                    className={style.data}
+                    style={
+                        +moment().format('e') === 5 ||
+                        +moment().format('e') === 6
+                            ? {color: 'red'}
+                            : {color: 'black'}
+                    }
+                >
+                    {moment().format('MMMM Do YYYY, HH:mm')}
+                </div>
+                <div className={style.today}>сейчас</div>
+                <div className={style.text}>
+                    Ветер: {wind.speed} м/с, {directWind(wind.deg)}{' '}
+                </div>
+                <div className={style.text}>
+                    Давление: {Math.floor(main.pressure * 0.75)} мм рт. ст.
+                </div>
+                <div className={style.text}>Влажность: {main.humidity} %</div>
+                <div className={style.text}>Температура:</div>
+                <div className={style.temperature}>
+                    <span className={style.minTemp}>
+                        {convertKtoC(main.temp_min)}
+                    </span>
+                    <span className={style.maxTemp}>
+                        {convertKtoC(main.temp_max)}
+                    </span>
+                </div>
+                <br/>
+                <div className={style.text}>
+                    Чувствуется: {convertKtoC(main.feels_like)}{' '}
+                </div>
+            </div>
+
+            <div className={style.iconContent}>
+                <img
+                    src={`http://openweathermap.org/img/wn/${weather[0].icon}@4x.png`}
+                    className={style.iconImg}
+                    alt={'Нет картинки'}
+                />
+                <div className={style.textImg}>{weather[0].description}</div>
+            </div>
+
+            <div className={style.longWeatherContent}>
+                <div
+                    className={style.dayWeather}
+                    style={
+                        +moment(list[start].dt * 1000).format('e') === 6 ||
+                        +moment(list[start].dt * 1000).format('e') === 0
+                            ? {color: 'red'}
+                            : {color: 'black'}
+                    }
+                >
+                    {dayWeather}
+                </div>
+                <div className={style.today}>{day()}</div>
+                <div className={style.longWeather}>
+                    {start !== 0 && (
+                        <button onClick={() => nextPrevWeather('prev')}>
+                            &#10094;
+                        </button>
+                    )}
+                    {list.slice(start, finish).map((pieceTime, index) => {
+                        const match = moment(pieceTime.dt * 1000).format(
+                            'ddd, D MMM'
+                        );
+                        const time = moment(pieceTime.dt * 1000).format(
+                            'HH:mm'
+                        );
+                        return (
+                            <div key={index} className={style.futureWeather}>
+                                <div>{match}</div>
+                                <div>{time}</div>
+                                <div>
+                                    t: {convertKtoC(pieceTime.main.temp)}{' '}
+                                </div>
+                                <img
+                                    src={`http://openweathermap.org/img/wn/${pieceTime.weather[0].icon}@2x.png`}
+                                    className={style.miniIconImg}
+                                    alt={pieceTime.weather[0].description}
+                                />
+                            </div>
+                        );
+                    })}
+                    {finish !== finishMax && (
+                        <button onClick={() => nextPrevWeather('next')}>
+                            &#10095;
+                        </button>
+                    )}
+                </div>
+            </div>
         </div>
-
-    </div>
-}
-export default Context
-
+    );
+};
+export default Context;
 
 // const months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 // const dayWeek = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ',];
 // const dayNow = new Date().getDay();
-
 
 // const weatherPic = {
 //     'clear sky': {backgroundPosition: '90% 16.6%'},
